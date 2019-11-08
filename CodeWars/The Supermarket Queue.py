12cd4f677c98b7f84041e1715713f3eb0bfc17a9
Ken Nguyen <ken_td@hotmail.com>
2019-11-08 20:39:42 +0100

    Update The Supermarket Queue.py

--- CodeWars/The Supermarket Queue.py
+++ CodeWars/The Supermarket Queue.py
@@ -1,1 +1,10 @@
-
+def queue_time(customers, n):
+    time = 0
+    time2 = 0
+    if n == 1:
+        for x in customers:
+            time += x
+    elif n >= len(customers):
+        return max(customers)
+    elif not customers:
+        return False
